63bdcad1e3921ffd537379e1f9b29e466b0c7038
MrUnluckyy <Justas.Sobutas@gmail.com>
2020-04-24 20:34:58 +0300

    php hw1 v1.0

---
+++ HomeWork/php01/index.php
@@ -0,0 +1,28 @@
+
+<!DOCTYPE html>
+<html lang="lt">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>PHP HW1 </title>
+    </head>
+
+    <body>
+        <?php
+            $country = 'Lietuva';
+            $city = 'Vilnius';
+            $population = 574221;
+
+        ?>
+
+        <ul>
+            <li> <?php echo "Šalis: $country" ?></li>
+            <li> <?php echo "Miestas: $city" ?></li>
+            <li> <?php echo "Populiacija: $population" ?></li>
+        </ul>
+
+    </body>
+</html>
+
+
+
