d55a5f7960dfc944e7c66ae8b10b90c137336446
MrUnluckyy <Justas.Sobutas@gmail.com>
2020-04-28 14:37:33 +0300

    php09 hw v1.0

---
+++ HomeWork/php09/index09.php
@@ -0,0 +1,19 @@
+<?php
+
+$ceu = array("Italija"=>"Roma", "Liuksemburgas"=>"Liuksemburgas", "Belgija"=> "Briuselis", "Danija"=>"Kopenhaga", "Suomija"=>"Helsinkis", "Prancūzija" => "Paryžius", "Slovakija"=>"Bratislava", "Slovenija"=>"Liublijana", "Vokietija" => "Berlynas", "Graikija" => "Atėnai", "Airija"=>"Dublinas", "Nyderlandai"=>"Amsterdamas", "Portugalija"=>"Lisabona", "Ispanija"=>"Madridas", "Švedija"=>"Stokholmas", "Didžioji Britanija"=>"Londonas", "Kipras"=>"Nikosija", "Lietuva"=>"Vilnius", "Čekija"=>"Praha", "Estija"=>"Talinas", "Vengrija"=>"Budapeštas", "Latvija"=>"Ryga", "Malta"=>"Valeta", "Austrija" => "Viena", "Bulgarija" => "Sofija", "Rumunija" => "Bukareštas", "Lenkija"=>"Varšuva");
+
+asort($ceu);
+
+echo '<ul>';
+
+foreach ($ceu as $country => $capital) {
+    echo "<li> Šalis: $country, Sostinė: $capital</li>";
+}
+
+
+echo '</ul>';
+
+
+
+?>
+
